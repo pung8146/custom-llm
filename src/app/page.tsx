c1885a19ff387155e4ac41f442bd15656400e945
pung8146 <pung8146@naver.com>
2025-06-16 16:07:18 +0900

    tailwind.config.js 파일의 경로 설정을 간소화하고, 색상 및 애니메이션 관련 속성을 일관된 따옴표 스타일로 수정했습니다. 또한, 페이지 구성에서 TestStyles 컴포넌트를 추가하여 Chat 컴포넌트와 함께 렌더링하도록 변경했습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@
 
 import Chat from '@/components/chat';
 import Sidebar from '@/components/sidebar';
+import TestStyles from '@/components/test-styles';
 
 export default function HomePage() {
   return (
     <div className="flex h-screen bg-white">
       <Sidebar />
-      <Chat />
+      <div className="flex-1 flex flex-col">
+        <TestStyles />
+        <Chat />
+      </div>
     </div>
   );
 }
