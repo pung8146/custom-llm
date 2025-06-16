98b2039c81deb65fe6c19ac340cf0598f200935f
pung8146 <pung8146@naver.com>
2025-06-17 05:24:22 +0900

    TestStyles 컴포넌트를 삭제하고, 페이지에서 Chat 컴포넌트와 Sidebar 컴포넌트의 임포트 문법을 일관된 따옴표 스타일로 수정했습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-'use client';
+"use client";
 
-import Chat from '@/components/chat';
-import Sidebar from '@/components/sidebar';
-import TestStyles from '@/components/test-styles';
+import Chat from "@/components/chat";
+import Sidebar from "@/components/sidebar";
 
 export default function HomePage() {
   return (
     <div className="flex h-screen bg-white">
       <Sidebar />
       <div className="flex-1 flex flex-col">
-        <TestStyles />
         <Chat />
       </div>
     </div>
